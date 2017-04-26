78c06b0ee7a19ef81dfe3258f20ead60e118811a
Marc Brückner <m.brueckner90@gmail.com>
2017-04-26 10:17:57 +0200

    host keys

--- ssh_rsync_host/docker-entrypoint.sh
+++ ssh_rsync_host/docker-entrypoint.sh
@@ -3,13 +3,24 @@
 #on first run generate host keys
 if [ ! -f "/etc/ssh/ssh_host_rsa_key" ]; then
         # generate fresh rsa key
-        ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+        ssh-keygen -t rsa -f /etc/ssh/ssh_host_rsa_key -N ''
 fi
 
 if [ ! -f "/etc/ssh/ssh_host_dsa_key" ]; then
         # generate fresh dsa key
-        ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+        ssh-keygen -t dsa -f /etc/ssh/ssh_host_dsa_key -N ''
 fi
+
+if [ ! -f "/etc/ssh/ssh_host_ecdsa_key" ]; then
+        # generate fresh dsa key
+        ssh-keygen -t ecdsa -f /etc/ssh/ssh_host_ecdsa_key -N ''
+fi
+
+if [ ! -f "/etc/ssh/ssh_host_ed25519_key" ]; then
+        # generate fresh dsa key
+        ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ''
+fi
+
 
 #set authorized keys
 cat /keys/* > /root/.ssh/authorized_keys
